8db1454bf405369d0f9677fdf94942cb4446afb4
Jakob Ankarhem <jakob@ankarhem.dev>
2023-12-02 12:21:47 +0100

    part2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,13 +3,14 @@ advent_of_code::solution!(2);
 use lazy_static::lazy_static;
 use regex::Regex;
 
-struct Condition {
+struct Game {
+    id: u32,
     max_red: u32,
     max_green: u32,
     max_blue: u32,
 }
 
-fn process_line(s: &str, cond: &Condition) -> Option<u32> {
+fn process_line(s: &str) -> Game {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"Game (?<id>\d+):").unwrap();
     }
@@ -44,31 +45,47 @@ fn process_line(s: &str, cond: &Condition) -> Option<u32> {
         .max()
         .unwrap_or(0);
 
-    if reds > cond.max_red || greens > cond.max_green || blues > cond.max_blue {
-        return None;
+    Game {
+        id: *id,
+        max_red: reds,
+        max_green: greens,
+        max_blue: blues,
     }
-
-    Some(*id)
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let cond = Condition {
-        max_red: 12,
-        max_green: 13,
-        max_blue: 14,
-    };
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
 
     let sum = input
         .lines()
         .filter(|l| !l.is_empty())
-        .filter_map(|line| process_line(line, &cond))
+        .filter_map(|line| {
+            let game = process_line(line);
+
+            if game.max_red > max_red || game.max_green > max_green || game.max_blue > max_blue {
+                return None;
+            }
+
+            Some(game.id)
+        })
         .sum();
 
     Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let power = input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|line| {
+            let game = process_line(line);
+            game.max_red * game.max_green * game.max_blue
+        })
+        .sum();
+
+    Some(power)
 }
 
 #[cfg(test)]
@@ -91,25 +108,38 @@ mod tests {
     #[case("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5)]
     // Extra cases
     #[case("Game 100: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 100)]
-    fn test_part_two_lines(#[case] input: &str, #[case] expected: u32) {
+    fn test_part_one_lines(#[case] input: &str, #[case] expected: u32) {
         let result = part_one(input);
         assert_eq!(expected, result.unwrap())
     }
 
     #[test]
     fn test_part_one() {
-        let example_part_one = r"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        let result = part_one(example_part_one);
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(8, result.unwrap());
+    }
+
+    #[rstest]
+    #[case("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48)]
+    #[case("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12)]
+    #[case(
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        1560
+    )]
+    #[case(
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        630
+    )]
+    #[case("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)]
+    // Extra cases
+    fn test_part_two_lines(#[case] input: &str, #[case] expected: u32) {
+        let result = part_two(input);
+        assert_eq!(expected, result.unwrap())
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(2286, result.unwrap());
     }
 }
