614357b1c1261399a6c08451d02e5852b804a808
Jakob Ankarhem <jakob@ankarhem.dev>
2023-12-09 15:56:57 +0100

    skip winnow parser, better performance without

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,27 +1,13 @@
 use rayon::prelude::*;
-use winnow::ascii::line_ending;
-use winnow::prelude::*;
-use winnow::{
-    ascii::{dec_int, space1},
-    combinator::separated,
-    PResult,
-};
 
 advent_of_code::solution!(9);
 
-// -- parser --
-fn digit_parser(input: &mut &str) -> PResult<i64> {
-    dec_int.parse_next(input)
-}
-
-fn line_parser(input: &mut &str) -> PResult<Vec<i64>> {
-    separated(0.., digit_parser, space1)
-        .map(|s| s)
-        .parse_next(input)
-}
-
-fn input_parser(input: &mut &str) -> PResult<Vec<Vec<i64>>> {
-    separated(0.., line_parser, line_ending).parse_next(input)
+fn parse_input(input: &str) -> impl ParallelIterator<Item = Vec<i64>> + '_ {
+    input.par_lines().filter(|l| !l.is_empty()).map(|l| {
+        l.split_ascii_whitespace()
+            .map(|s| s.parse::<i64>().expect("To be able to parse number `{l}`"))
+            .collect::<Vec<_>>()
+    })
 }
 
 // -- utility --
@@ -46,11 +32,8 @@ fn next_number(vec: &[i64]) -> i64 {
 }
 
 pub fn part_one(input: &str) -> Option<i64> {
-    let input = input_parser
-        .parse(input)
-        .map_err(|e| e.to_string())
-        .unwrap();
-    let sum = input.par_iter().map(|v| next_number(v)).sum();
+    let input = parse_input(input);
+    let sum = input.map(|v| next_number(&v)).sum();
 
     Some(sum)
 }
@@ -68,11 +51,8 @@ fn previous_number(vec: &[i64]) -> i64 {
 }
 
 pub fn part_two(input: &str) -> Option<i64> {
-    let input = input_parser
-        .parse(input)
-        .map_err(|e| e.to_string())
-        .unwrap();
-    let sum = input.par_iter().map(|v| previous_number(v)).sum();
+    let input = parse_input(input);
+    let sum = input.map(|v| previous_number(&v)).sum();
 
     Some(sum)
 }
@@ -82,30 +62,6 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
     use rstest::rstest;
-
-    #[rstest]
-    #[case("0 3 6 9 12 15", vec![0, 3, 6, 9, 12, 15])]
-    #[case("1 3 6 10 15 21", vec![1, 3, 6, 10, 15, 21])]
-    #[case("10 13 16 21 30 45", vec![10, 13, 16, 21, 30, 45])]
-    #[case("-10 2 -4", vec![-10, 2, -4])]
-    fn test_line_parser(#[case] mut input: &str, #[case] expected: Vec<i64>) {
-        let result = line_parser.parse_next(&mut input);
-        assert_eq!(expected, result.unwrap());
-    }
-
-    #[test]
-    fn test_input_parser() {
-        let input = r"0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45";
-        let expected = vec![
-            vec![0, 3, 6, 9, 12, 15],
-            vec![1, 3, 6, 10, 15, 21],
-            vec![10, 13, 16, 21, 30, 45],
-        ];
-
-        assert_eq!(expected, input_parser.parse(input).unwrap());
-    }
 
     #[rstest]
     #[case(vec![3, 3, 3, 3, 3], true)]
